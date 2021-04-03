97334f1f3bb1c119f780f1f4f724d3cda8bfd786
Eamon <eamon0989@gmail.com>
2021-04-03 14:53:46 +0200

    Refactored code and functions

---
+++ function2.php
@@ -0,0 +1,76 @@
+<?php
+
+$password; //the correct pin will be stored here and returned to user
+$show = 15; //amount of attempts to print
+$checks = 0; //how many hashes were checked
+
+
+
+function codecracker1($input, $md5) { //this function takes the input and cracks the code, then returns it to the other function
+    global $show, $password, $checks;
+    for($i = 0; $i < strlen($input); $i++) {
+        $ch1 = $input[$i];
+            for($j = 0; $j < strlen($input); $j++) {
+                $ch2 = $input[$j];
+                    for($k = 0; $k < strlen($input); $k++) {
+                        $ch3 = $input[$k];
+                            for($l = 0; $l < strlen($input); $l++) {
+                                $ch4 = $input[$l];
+                                $checks++; // adds to counter every time a hash is checked
+                            
+                                $guess = $ch1.$ch2.$ch3.$ch4; //concatinates 4 chars together
+                                $attempt = hash('md5', $guess);// checks current string's hash against user's md5 hash
+                            
+                                if ($attempt == $md5) {
+                                    $password = $guess; //stores the current (correct) guess as the password
+                                    return $password;
+                                    break;
+                                    }
+                                if ( $show > 0 ) {
+                                    print "$attempt $guess\n"; // prints the first few attempts until counter reaches 0
+                                    $show = $show - 1;
+                                    }
+                                
+                                }
+                        }
+                }
+        }
+}
+/*
+This function is here to optimize the overall workings, if it checks the pin for symbols it takes a long time, even if
+the pin is simple and only contains numbers. It checks in increasing order of complexity, first numbers, then letters, then symbols.
+It alse keeps track of the total time and returns the correct password to the user
+*/
+function codecracker() { 
+    global $password, $checks;
+
+    $numbers ="0123456789";
+    $numbersandletters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $numletandsymb = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&/()=?¿'¡*+[]}{;:_,.-<>|";
+
+    $time_pre = microtime(true);
+    $md5 = $_GET['md5'];
+    codecracker1($numbers, $md5);
+        if ($password != true) {
+            codecracker1($numbersandletters, $md5);
+                if ($password != true) {
+                    codecracker1($numletandsymb, $md5);
+                    if ($password != true) {
+                        print "I can't crack this pin";
+                        print "\n";  
+                    }
+            }
+        }
+    $time_post = microtime(true);
+    print "Elapsed time: ";
+    print $time_post-$time_pre; //working
+    print "\n";  
+
+    if ($password !=false) {
+        print "\n";  
+        print "Total number of hashes checked: $checks";
+        print "\n"; 
+        print "<strong>Your pin was: $password</strong>"; 
+    }
+}
+
--- index.php
+++ index.php
@@ -5,97 +5,62 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php    require'function2.php'; ?>
 </head>
 <body>
 
-<h1>md5 pin hasher:</h1>
-<p>Hash your 4 digit pin here: (numbers only)</p>
+    <h1>md5 pin hasher:</h1>
+    <p>Hash your 4 digit pin here: </p>
 
-<form>
-    <input type="text" name="md5hash" size="60" />
-    <input type="submit" value="Make MD5"/>
-</form>
+    <form>
+        <input type="text" name="md5hash" size="60" />
+        <input type="submit" value="Make MD5"/>
+    </form>
 
-<?php
-    if ( isset($_GET['md5hash']) ) {
-$md5hash = $_GET['md5hash'];
-    }
-?>
-<pre>
-Copy and paste this hash into the second box.
-
-It will check the hashes of the numbers 0000 to 1000 to see which matches your pin.
-</pre>
-<?php
-    if ( isset($_GET['md5hash']) ) {
-    print hash('md5', $md5hash);
-    }
-    
+    <?php
+        if ( isset($_GET['md5hash']) ) {
+    $md5hash = $_GET['md5hash'];
+        }
     ?>
-<pre>
+    <pre>
+    Copy and paste this hash into the second box.
 
-
-
-</pre>
-<form>
-<input type="text" name="md5" size="60" />
-<input type="submit" value="Crack MD5"/>
-</form>
-<h2>First 15 attempts:</h2>
-<pre>
-<?php
-
-$goodtext = "Not found";
-
-    if ( isset($_GET['md5']) ) {
-        $time_pre = microtime(true);
-        $md5 = $_GET['md5']; 
-
-        $numbers = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!|@#·$~%&¬/()=?¿'¡*+[]ç}{;:_,.-<>";
-        $show = 15;
-        $checks = 0;
-
-        
-        for($i = 0; $i < strlen($numbers); $i++) {
-            $ch1 = $numbers[$i];
-           for($j = 0; $j < strlen($numbers); $j++) {
-            $ch2 = $numbers[$j];
-           for($k = 0; $k < strlen($numbers); $k++) {
-            $ch3 = $numbers[$k];
-           for($l = 0; $l < strlen($numbers); $l++) {
-            $ch4 = $numbers[$l];
-            $checks++;
-        
-
-        $guess = $ch1.$ch2.$ch3.$ch4;
-
-        $check = hash('md5', $guess);
-        if ($check == $md5) {
-            $goodtext = $guess;
-            print "Total number of hashes checked: $checks";
-            print "\n";
-            break;
+    It will check the hashes of the numbers 0000 to 9999 to see which matches your pin.
+    Only numbers will solve in less than 1 second.
+    Letters and numbers will take less than 5 seconds.
+    Letters and common symbols will take up to 30 seconds.
+    </pre>
+    <?php
+        if ( isset($_GET['md5hash']) ) {
+        print hash('md5', $md5hash);
         }
- 
-        if ($show > 0) {
-            print "$check $guess\n";
-            $show = $show - 1;
-        }
-    }
-}
-           }
-        }
-           $time_post = microtime(true);
-           print "Elapsed time: ";
-           print $time_post-$time_pre;
-           print "\n";
-        
-    }
 
-?>
-</pre>
+    ?>
+    <pre>
 
-<p><strong>Your Pin was: <?= htmlentities($goodtext); ?></strong></p>
+
+
+    </pre>
+    <form>
+        <input type="text" name="md5" size="60" />
+        <input type="submit" value="Crack MD5"/>
+    </form>
+    <h2>First 15 attempts:</h2>
+
+    <pre>
+        <?php
+        if ( isset($_GET['md5']) ) {
+            codecracker();
+        }
+        ?>
+    </pre>
+
+    <div style="height: 15rem; overflow:auto;">
+    <?php
+
+    ?>
+    </div>
+
 
 </body>
 </html>
