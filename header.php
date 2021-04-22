9445aeff89c82bb53c5556c8777734a89cc99cc7
Eamon <eamon0989@gmail.com>
2021-04-22 09:31:11 +0200

    fixed mobile overscroll bug

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Md5 Cracker</title>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+<title>Md5 Cracker</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"> 
     <link rel="stylesheet" href="style.css">   
     <?php    require'function.php'; ?>
